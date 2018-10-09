275630f8c4f00e9902832455cafcfdb9d29dfc9b
John Paul Depew <jpdepewjp@gmail.com>
2018-10-09 17:44:18 -0400

    Added scenes, singleton, and dorky moving between scenes.

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Constants : MonoBehaviour
+{
+    public string playerPrefHighScoreKey = "playerHighScore";
+    public int score = 0;
+    static public Constants S;
+
+    private void Awake()
+    {
+        S = this;
+
+        if (PlayerPrefs.HasKey(playerPrefHighScoreKey))
+        {
+            score = PlayerPrefs.GetInt(playerPrefHighScoreKey);
+        }
+        else
+        {
+            Debug.Log(score);
+            PlayerPrefs.SetInt(playerPrefHighScoreKey, score);
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/SplashSceneManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SplashSceneManager : MonoBehaviour
+{
+    public Text highScoreText;
+
+    float timer = 0;
+
+    private void Start()
+    {
+        highScoreText.text = "Score: " + Constants.S.score.ToString();
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 1)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+        }
+    }
+}
