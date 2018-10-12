1c2c959ecd43e2b969df24b71172bbd5d5e47cdd
John Paul Depew <jpdepewjp@gmail.com>
2018-10-11 23:07:21 -0400

    Added block image + block script

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+    public int health = 1;
+
+    SpriteRenderer spriteRenderer;
+
+	void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+    }
+	
+	void Update () {
+		
+	}
+
+    IEnumerator DestroyThis()
+    {
+        spriteRenderer.color = new Color(1, 1, 1, 0);
+        yield return new WaitForSeconds(0.5f);
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Ball")
+        {
+            health--;
+            if(health <= 0)
+            {
+                
+            }
+        }
+    }
+}
